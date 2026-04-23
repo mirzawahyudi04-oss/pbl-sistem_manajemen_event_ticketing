0fb11731b5a53b3feea50ee60f5568623e8288d0
raffi271104 <mraffifauzi@gmail.com>
2026-04-23 21:29:49 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-
 Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/register', function () {
     return view('auth.register');
