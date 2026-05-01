5e7b27df388a46d125d7c5389544077f4160be03
muhammad mirza faaris wahyudi <mirza.wahyudi04@gmail.com>
2026-05-01 23:17:13 +0700

    ubah dikit

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -164,7 +164,7 @@
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/">LOGO</a>
+        <a class="navbar-brand" href="/">STEVENtix</a>
         <div class="d-flex align-items-center gap-3">
             <a class="nav-link text-white" href="/">Beranda</a>
             <a class="nav-link text-white" href="#">Jelajahi Event</a>
